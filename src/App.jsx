e4d8b4652f004db67e69dcd06d68d8e705988d2e
alpinmohsa541 <alpinmohsa@gmail.com>
2025-01-04 16:22:50 +0700

    Feat :  Membuat page dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./Login/LoginForm"; // Sesuaikan path jika berbeda
 import Register from "./Register/RegisterForm"; // Sesuaikan path jika berbeda
 import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap-icons/font/bootstrap-icons.css";
 import Reset from "./ResetPassword/EmailForm";
 import ResetPassword from "./ResetPassword/ResetPasswordForm";
 import CashierDashboard from "./CashierDashboard/CashierDashboard";
 import SalesReport from "./SalesReport/SalesReport";
 import Profile from "./Profile/Profile"; // Import Profile page
+import Dashboard from "./Dashboard/Dashboard";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
 function App() {
@@ -26,7 +28,9 @@ function App() {
         {/* Route untuk halaman Sales Report */}
         <Route path="/sales-report" element={<SalesReport />} />
         {/* Route untuk halaman Profile */}
-        <Route path="/profile" element={<Profile />} />{" "}
+        <Route path="/profile" element={<Profile />} />
+        {/* Route untuk halaman dashboard */}
+        <Route path="/dashboard" element={<Dashboard />} />{" "}
       </Routes>
     </Router>
   );
